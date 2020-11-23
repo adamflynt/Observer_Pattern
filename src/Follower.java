92fb5b1e04c6b9030384e5d2acb1466a1b97253e
Adam Flynt <adam.e.flynt@gmail.com>
2020-11-23 15:04:41 -0600

    first commit

---
+++ src/Follower.java
@@ -0,0 +1,25 @@
+
+public class Follower implements Observer {
+	
+	private String followerName;
+
+	public Follower(String followerName) {
+		this.followerName = followerName;
+	}
+
+	public String getFollowerName() {
+		return followerName;
+	}
+
+	public void setFollowerName(String followerName) {
+		this.followerName = followerName;
+	}
+
+	public void update(String status) {
+		// let followers know channel is live
+	}
+
+	public void play() {
+		// play channel
+	}
+}
